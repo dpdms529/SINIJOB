93ac36547b43814e038a91a6ab089e684d2d56b0
조예은 <musiclove529@gmail.com>
2021-07-27 17:35:53 +0900

    Revert "Merge branch 'feature/#12/yena' into 'master'"
    This reverts merge request !3

--- Hanieum/app/src/main/java/org/techtown/hanieum/OnRecoItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.techtown.hanieum;
-
-import android.view.View;
-
-public interface OnRecoItemClickListener {
-    public void OnItemClick(RecommendAdapter.ViewHolder holder, View view, int position);
-}
