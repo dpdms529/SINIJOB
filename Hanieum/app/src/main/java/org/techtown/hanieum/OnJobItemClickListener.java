a6a5f12c4ee25e6eee3f6fae4491320e0aee4cf0
yegrina <syn5867@naver.com>
2021-07-27 18:07:40 +0900

    feat: Detail screen implementaion and job select 1. 상세정보 화면 구현 및 데이터 불러오기 2. 직종 선택 화면에서 맞는 직종 표시 Feat #12, #1

---
+++ Hanieum/app/src/main/java/org/techtown/hanieum/OnJobItemClickListener.java
@@ -0,0 +1,7 @@
+package org.techtown.hanieum;
+
+import android.view.View;
+
+public interface OnJobItemClickListener {
+    public void OnItemClick(JobAdapter.Job1ViewHolder holder, View view, int position);
+}
