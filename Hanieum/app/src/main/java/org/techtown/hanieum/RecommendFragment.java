63c99f98618d6a4f37257735d8a2ff91800e88b4
YE-EUN <musiclove529@naver.com>
2021-07-30 06:20:46 +0900

    feat: Add file connecting download link function & researching recruit item function
    1. 제출서류양식 다운로드 링크 연결
    2. 공고 검색 기능(공고 제목 또는 회사이름으로 검색)
    - 검색 완료 후 키보드 내리고 검색창 없애는 기능
    
    feat: #12

--- Hanieum/app/src/main/java/org/techtown/hanieum/RecommendFragment.java
+++ Hanieum/app/src/main/java/org/techtown/hanieum/RecommendFragment.java
@@ -1,18 +1,26 @@
 package org.techtown.hanieum;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.collection.ArraySet;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +38,13 @@ public class RecommendFragment extends Fragment implements View.OnClickListener 
     ImageButton searchButton; // 검색 버튼
     ImageButton menuButton; // 메뉴 버튼
     TextView itemNum;
+    TextView title; //화면 제목
+    EditText editSearch;  //검색창
+
+    ArrayList<Recommendation> items; //리사이클러뷰에서 보여줄 공고 아이템을 저장하고 있는 ArrayList
+    ArrayList<Recommendation> allItems = new ArrayList<>(); //전체 공고 아이템을 저장하고있는 ArrayList
+
+    InputMethodManager imm;
 
     public RecommendFragment() {
         // Required empty public constructor
@@ -50,6 +65,10 @@ public class RecommendFragment extends Fragment implements View.OnClickListener 
         searchButton = view.findViewById(R.id.searchButton);
         menuButton = view.findViewById(R.id.menuButton);
         itemNum = view.findViewById(R.id.itemNum);
+        title = view.findViewById(R.id.title);
+        editSearch = view.findViewById(R.id.editSearch);
+
+        imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
         // 리사이클러뷰와 어댑터 연결
         LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(),
@@ -75,6 +94,39 @@ public class RecommendFragment extends Fragment implements View.OnClickListener 
 
         loadListData();
 
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String text = editSearch.getText().toString();
+                search(text);
+            }
+        });
+
+        editSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE){
+                    title.setVisibility(View.VISIBLE);
+                    editSearch.setVisibility(View.GONE);
+
+                    imm.hideSoftInputFromWindow(editSearch.getWindowToken(),0); //키보드 내리기
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
         return view;
     }
 
@@ -85,13 +137,23 @@ public class RecommendFragment extends Fragment implements View.OnClickListener 
             startActivity(intent);
         } else if (v == searchButton) {
             Toast.makeText(v.getContext(), "검색 버튼 눌림", Toast.LENGTH_LONG).show();
+            if(title.getVisibility()==View.VISIBLE){
+                title.setVisibility(View.GONE);
+                editSearch.setVisibility(View.VISIBLE);
+            }else{
+                title.setVisibility(View.VISIBLE);
+                editSearch.setVisibility(View.GONE);
+                imm.hideSoftInputFromWindow(editSearch.getWindowToken(),0);
+            }
+
+
         } else if (v == menuButton) {
             Toast.makeText(v.getContext(), "메뉴 버튼 눌림", Toast.LENGTH_LONG).show();
         }
     }
 
     private void loadListData() { // 항목을 로드하는 함수
-        ArrayList<Recommendation> items = new ArrayList<>();
+        items = new ArrayList<>();
 
         String list = getResources().getString(R.string.serverIP)+"recruit.php";
         URLConnector task = new URLConnector(list);
@@ -129,5 +191,21 @@ public class RecommendFragment extends Fragment implements View.OnClickListener 
         }
 
         adapter.setItems(items);
+        allItems.addAll(items);
+    }
+
+    public void search(String text){    //공고 제목 또는 회사명으로 검색하는 함수
+        items.clear();
+        if(text.length() == 0){ //검색어 없을 때 전체 데이터 보여줌
+            items.addAll(allItems);
+        }else{  //검색어 있을 때 검색어를 포함하는 데이터만 보여줌
+            for(int i = 0;i<allItems.size();i++){
+                if(allItems.get(i).getTitle().contains(text) || allItems.get(i).getCompanyName().contains(text)){
+                    items.add(allItems.get(i));
+                }
+            }
+            itemNum.setText(String.valueOf(items.size()));
+        }
+        adapter.notifyDataSetChanged();
     }
 }
