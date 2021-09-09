9f331bb5e24c85e28a65291997bbe4cfa5597d43
YE-EUN <musiclove529@naver.com>
2021-09-09 21:39:36 +0900

    room version up

--- Hanieum/app/src/main/java/org/techtown/hanieum/db/AppDatabase.java
+++ Hanieum/app/src/main/java/org/techtown/hanieum/db/AppDatabase.java
@@ -22,7 +22,7 @@ import org.techtown.hanieum.db.entity.Recruit;
 import org.techtown.hanieum.db.entity.RecruitCertificate;
 
 
-@Database(entities = {JobCategory.class, Bdong.class, Recruit.class, RecruitCertificate.class, CvInfo.class, CoverLetter.class, Certificate.class}, version = 6, exportSchema = false)
+@Database(entities = {JobCategory.class, Bdong.class, Recruit.class, RecruitCertificate.class, CvInfo.class, CoverLetter.class, Certificate.class}, version = 7, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract JobCategoryDao jobCategoryDao();
 
