4b5ed80c0b36e2ca100b0deb2b49463fc987a066
YE-EUN <musiclove529@naver.com>
2021-09-23 05:07:16 +0900

    Add : kakao,google login & logout & diconnect
    1. 카카오 로그인, 로그아웃, 탈퇴
    2. 구글 로그인, 로그아웃, 탈퇴
    
    Add : #45

--- Hanieum/app/src/main/java/org/techtown/hanieum/InformationFragment.java
+++ Hanieum/app/src/main/java/org/techtown/hanieum/InformationFragment.java
@@ -1,64 +1,100 @@
 package org.techtown.hanieum;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link InformationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class InformationFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kakao.sdk.user.UserApiClient;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import org.jetbrains.annotations.NotNull;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class InformationFragment extends Fragment implements View.OnClickListener {
+    Context context;
+    Button logoutBtn,disconnectBtn;
 
     public InformationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InformationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InformationFragment newInstance(String param1, String param2) {
-        InformationFragment fragment = new InformationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_information, container, false);
+        context = getContext();
+        logoutBtn = view.findViewById(R.id.logout_btn);
+        disconnectBtn = view.findViewById(R.id.disconnect_btn);
+        logoutBtn.setOnClickListener(this);
+        disconnectBtn.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == logoutBtn){
+//            //구글 로그아웃
+//            FirebaseAuth.getInstance().signOut();
+//            Log.d("TAG", "구글 사용자 로그아웃");
+//            Intent intent = new Intent(context, LoginActivity.class);
+//            startActivity(intent);
+//            getActivity().finish();
+
+            //카카오 로그아웃
+            UserApiClient.getInstance().logout((error) -> {
+                if(error != null){
+                    Log.e("TAG", "카카오 로그아웃 실패. SDK에서 토큰 삭제됨", error);
+                }else{
+                    Log.i("TAG", "카카오 로그아웃 성공. SDK에서 토큰 삭제됨");
+                }
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+                return  null;
+            });
+        }else if(v == disconnectBtn){
+//            //구글 탈퇴
+//            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//            user.delete()
+//                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        @Override
+//                        public void onComplete(@NonNull @NotNull Task<Void> task) {
+//                            if(task.isSuccessful()){
+//                                Log.d("TAG", "구글 사용자 탈퇴");
+//                                Intent intent = new Intent(context, LoginActivity.class);
+//                                startActivity(intent);
+//                                getActivity().finish();
+//                            }
+//                        }
+//                    });
+
+            //카카오 탈퇴
+            UserApiClient.getInstance().unlink((error) -> {
+                if(error != null){
+                    Log.e("TAG", "카카오 탈퇴 실패", error);
+                }else{
+                    Log.i("TAG", "카카오 탈퇴 성공. SDK에서 토큰 삭제 됨");
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+                return null;
+            });
+        }
     }
 }
