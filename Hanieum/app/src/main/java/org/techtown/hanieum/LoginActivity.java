69cefd8318ac29661b18e421d46ef87b65fee27d
YE-EUN <musiclove529@naver.com>
2021-10-17 12:24:40 +0900

    remove import

--- Hanieum/app/src/main/java/org/techtown/hanieum/LoginActivity.java
+++ Hanieum/app/src/main/java/org/techtown/hanieum/LoginActivity.java
@@ -9,10 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
